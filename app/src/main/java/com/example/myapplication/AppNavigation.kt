596d05155b304babef6b7aac6dd0506e88a05725
LETICIAJUSTINOWEHBE <lehjustinowehbe@gmail.com>
2026-09-18 09:59:07 -0300

    first commit

---
+++ app/src/main/java/com/example/myapplication/AppNavigation.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHost
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavigation(){
+    // controller
+    val navController = rememberNavController()
+
+    // rotas
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable ("home") {
+            // Abrir tela principal
+            TelaPrincipal()
+        }
+
+        composable ("segundaTela") {
+            // Abrir a segunda tela
+        }
+    }
+}
